19d5d63ffc05d2e360aba0eb6af59cbc9651f19b
marcos.guedes <marcos.guedes@WIN-VYU8D20CMD>
2017-02-16 17:05:32 -0300

    Add project files.

---
+++ NavigationTest/NavigationTest/UiContentPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationTest
+{
+    public partial class UiContentPage : ContentPage
+    {
+        public UiContentPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async void OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new UiTabPage());
+        }
+    }
+}
---
+++ NavigationTest/NavigationTest/UiTabPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace NavigationTest
+{
+    public partial class UiTabPage : TabbedPage
+    {
+        public UiTabPage()
+        {
+            InitializeComponent();
+        }
+
+        protected async void OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new UICarouselPage());
+        }
+    }
+}
